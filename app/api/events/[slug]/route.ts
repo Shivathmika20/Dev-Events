735cb1b81ff026eabec2bb44840e47308f481ff0
Shivathmika20 <yelurishivathmika@gmail.com>
2025-12-09 16:22:49 +0530

    feat: added route handling logic for getting event from database based on the provided slug

--- app/api/events/[slug]/route.ts
+++ app/api/events/[slug]/route.ts
@@ -1,8 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server';  
 import {prisma} from '@/lib/prisma';
-export default async function GET({params}:{params:{slug:string}}){
+export async function GET(req: NextRequest, {params}:{params:Promise<{slug:string}>}) {
     try{
+        
+        const {slug} = await params
+        
+        //validation
+        if(!slug || typeof slug !== 'string'){
+            return NextResponse.json({message:"Invalid Slug Provided"}, {status:400});
+        }
 
+        const eventInfo=await prisma.event.findUnique({
+            where:{slug},
+            
+        })
+        if(!eventInfo){
+            return NextResponse.json({message:"Event not found"}, {status:404});
+        }
+        console.log(eventInfo);
+        return NextResponse.json({message:"Event found",eventInfo},{status:200});
     }
     catch(e){
         return NextResponse.json({message:"Couldnt find the Event"}, {status:500});
